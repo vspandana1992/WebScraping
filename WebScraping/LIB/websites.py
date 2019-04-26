cff7d8c98ed1c7bfd1e98be8eb901b2f36e8843c
vspandana1992 <v.spandana12@gmail.com>
2019-04-25 19:32:13 -0500

    Added Flickr scraping

--- WebScraping/LIB/websites.py
+++ WebScraping/LIB/websites.py
@@ -6,9 +6,10 @@ class _unsplash(object):
     def __init__(self):
         self.website = "https://www.unsplash.com"
         self.session = requests.Session()
-    def __call__(self,search):
+    def __call__(self,search,pages=None):
         base_url   = self.website+"/napi/search/photos?query={0}&xp=&per_page=20&".format(search)
-        pages      = self.session.get(base_url).json()['total_pages']
+        if not pages:
+            pages      = self.session.get(base_url).json()['total_pages']
         urls=[]
         for page in trange(1,pages+5,desc = "Downloading image URLs"):
             search_url  = self.website+"/napi/search/photos?query={0}&xp=&per_page=20&page={1}".format(search,page)
@@ -24,11 +25,12 @@ class _stocksnap(object):
         def __init__(self):
             self.website = "https://www.stocksnap.io"
             self.session = requests.Session()
-        def __call__(self,search):
+        def __call__(self,search,pages=None):
             base_url   = self.website+"/api/search-photos/{0}/relevance/desc/{1}"
             urls       = []
-            total       = self.session.get(base_url.format(search,1)).json()['count']
-            for page in trange(1,int(math.ceil(total/40.))+1):
+            if not pages:
+                pages       = self.session.get(base_url.format(search,1)).json()['count']
+            for page in trange(1,int(math.ceil(pages/40.))+1):
                 search_url  = base_url.format(search,page)
                 response    = self.session.get(search_url)
                 if response.status_code == 200 :
@@ -38,3 +40,24 @@ class _stocksnap(object):
 
 stocksnap  = _stocksnap()
 ## Usage : stocksnap(<search_term>) ; example : stocksnap('mushroom')
+
+class _flickr(object):
+    def __init__(self):
+        self.website = "https://api.flickr.com"
+        self.session = requests.Session()
+    def __call__(self,search,pages=None):
+        base_url   = self.website+"/services/rest?sort=relevance&parse_tags=1&content_type=7&extras=can_comment%2Ccount_comments%2Ccount_faves%2Cdescription%2Cisfavorite%2Clicense%2Cmedia%2Cneeds_interstitial%2Cowner_name%2Cpath_alias%2Crealname%2Crotation%2Curl_c%2Curl_l%2Curl_m%2Curl_n%2Curl_q%2Curl_s%2Curl_sq%2Curl_t%2Curl_z&per_page=25&page={1}&lang=en-US&text={0}&viewerNSID=&method=flickr.photos.search&csrf=&api_key=94378a1af11a0c3e68bc56704192ad50&format=json&hermes=1&hermesClient=1&reqId=539bc2ce&nojsoncallback=1"
+        urls       = []
+        if not pages:
+            pages       = self.session.get(base_url.format(search,1)).json()['photos']['pages']
+        for page in trange(1,pages, desc = "Downloading image URLs"):
+            search_url  = base_url.format(search,page)
+            response    = self.session.get(search_url)
+            if response.status_code == 200 :
+                try:
+                    results = response.json()
+                    urls    = urls + [(url['id'],url['url_m']) for url in results['photos']['photo']]
+                except:
+                    pass
+        return list(set(urls))
+flickr = _flickr()
--- WebScraping/scrape.py
+++ WebScraping/scrape.py
@@ -6,24 +6,25 @@ import argparse
 
 class _scrape(object):
     def __call__(self,args):
-        if args.w.lower() == 'unsplash' : urls = unsplash(args.q.lower())
-        if args.w.lower() == 'stocksnap'   : urls = stocksnap(args.q.lower())
+        if args.w.lower() == 'unsplash' : urls = unsplash(args.q.lower(), args.p)
+        if args.w.lower() == 'stocksnap'   : urls = stocksnap(args.q.lower(), args.p)
+        if args.w.lower() == 'flickr'   : urls = flickr(args.q.lower())
         download_imgs     = _download_imgs(args.o,args.q.lower())
         download_imgs(urls)
-
 scrape=_scrape()
 
 if __name__=='__main__':
     parser = argparse.ArgumentParser(description='Web Scraping')
-    parser.add_argument('-w', choices = ['unsplash','stocksnap'], metavar = 'website', required = True, type = str,
-                        help = 'name of the website that you want to scrape data from, example: unsplash,stocksnap')
+    parser.add_argument('-w', choices = ['unsplash','stocksnap','flickr'], metavar = 'website', required = True, type = str,
+                        help = 'name of the website that you want to scrape data from, example: unsplash,stocksnap,flickr')
     parser.add_argument('-q', metavar = 'query', required = True, type = str,
                         help = 'search term for query, example: mushroom')
     parser.add_argument('-o', metavar = 'output directory', required = True, type = str,
                         help = 'Path to the folder where you want the data to be stored, the directory will be created if not present')
+    parser.add_argument('-p', metavar = 'no of pages', type = int, help = 'Number of pages to scrape')
+
     args = parser.parse_args()
     scrape(args)
-
 
 ## command line usage:
 #  cd /WebScraping
